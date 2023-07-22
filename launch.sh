1db43843de4623b61235ef20da766e8797d72867
Vladimir Papazov <papazovvladimir88@gmail.com>
2023-07-22 18:49:27 +0300

    dpi fix

--- launch.sh
+++ launch.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 sel1=$(echo 'install
-uninstall' | rofi -dmenu -dpi 200)
+uninstall' | rofi -dmenu )
 
 if [ "$sel1" = "install" ]
 then
-	sel2=$(flatpak search "" --columns=application | rofi -dmenu -dpi 200)
+	sel2=$(flatpak search "" --columns=application | rofi -dmenu )
 	if [ "$sel2" != "" ]
 	then
-		echo "installing... You can close this window" | rofi -dmenu -dpi 200 &
+		echo "installing... You can close this window" | rofi -dmenu  &
 		flatpak install $sel2 -y
 		killall rofi
-		echo "installed" | rofi -dmenu -dpi 200 &
+		echo "installed" | rofi -dmenu  &
 	fi
 elif [ "$sel1" = "uninstall" ]
 then
-	sel2=$(flatpak list --columns=application | rofi -dmenu -dpi 200)
+	sel2=$(flatpak list --columns=application | rofi -dmenu )
 	if [ "$sel2" != "" ]
 	then
-		echo "uninstalling... You can close this window" | rofi -dmenu -dpi 200 &
+		echo "uninstalling... You can close this window" | rofi -dmenu  &
 		flatpak uninstall $sel2 -y
 		killall rofi
-		echo "uninstalled" | rofi -dmenu -dpi 200 &
+		echo "uninstalled" | rofi -dmenu  &
 	fi
 fi
